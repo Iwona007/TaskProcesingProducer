6154ac75dfceba5d67f7ad458953c8f02989e185
Iwona007 <iwonka.adamkiewicz@gmail.com>
2023-03-19 01:46:13 +0100

    add logic for database

---
+++ src/main/java/pl/iwona/TaskProcessingProducer/service/TaskService.java
@@ -0,0 +1,17 @@
+//package pl.iwona.TaskProcessingProducer.service;
+//
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface TaskService {
+//
+//    Task createTask(String pattern, String input);
+//
+//    List<Task> createListTask(String pattern, String input);
+//
+//
+//
+//    String readStatusAndResult(List<Task> taskList, String taskId);
+////TaskResultStatusDto readStatusAndResult(String taskId);
+//}
